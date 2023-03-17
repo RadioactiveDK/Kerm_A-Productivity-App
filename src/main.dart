5b25ce5ed0175802e1dfba78fc24ca245eb8ede2
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2023-03-17 10:36:36 +0530

    Update main.dart

--- src/main.dart
+++ src/main.dart
@@ -1,1 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kerm',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Colors.black87,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          )
+        )
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kerm'),
+        actions: <Widget> [
+          IconButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MySettings()),
+                );
+              },
+              icon: const Icon(Icons.settings),
+          )
+        ],
+      ),
+      body: const MyHomeBody(),
+    );
+  }
+}
+
+class MySettings extends StatefulWidget {
+  const MySettings({Key? key}) : super(key: key);
+
+  @override
+  State<MySettings> createState() => _MySettingsState();
+}
+class _MySettingsState extends State<MySettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        leading: BackButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+}
+
+class MyHomeBody extends StatefulWidget {
+  const MyHomeBody({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomeBody> createState() => _MyHomeBodyState();
+}
+class _MyHomeBodyState extends State<MyHomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(7),
+        child: Column(
+          children: const [
+            Text('data')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.black38,
+        child: const Icon(Icons.add,color: Colors.white,),
+      ),
+    );
+  }
+}
 
