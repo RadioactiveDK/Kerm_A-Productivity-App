4fa219ed75b40aafe9538bd7b1c3c4d16f0b5d65
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2023-03-24 15:12:08 +0530

    Update main.dart

--- src/main.dart
+++ src/main.dart
@@ -433,7 +433,7 @@ class _MyGoalsState extends State<MyGoals> {
 
   void updateGoals()async{
     KermDatabase kdb = KermDatabase();
-    await kdb.updateGoalData(goalsMap);
+    await kdb.updateGoalData();
     setState(() {});
   }
   updateState(){
@@ -555,6 +555,17 @@ class _MyGoalWidgetState extends State<MyGoalWidget> {
     return Column(children: milestoneWidgets);
   }
 
+  goalColor(String goalName){
+    if(goalsMap[goalName]!.isEmpty) return Colors.grey;
+    else{
+      if(goalsMap[goalName]!.firstWhere((element) => element[0]!='!',orElse: ()=>'0')=='0') {
+        return Colors.grey;
+      } else {
+        return Colors.cyanAccent;
+      }
+    }
+  }
+
   final myController = TextEditingController();
   @override
   void dispose() {
@@ -569,7 +580,7 @@ class _MyGoalWidgetState extends State<MyGoalWidget> {
       padding: const EdgeInsets.all(10),
       margin: const EdgeInsets.symmetric(vertical: 2),
       decoration: BoxDecoration(
-        color: (widget.goalName![0] == '!')?Colors.grey:Colors.cyanAccent,
+        color: goalColor(widget.goalName!),
         borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
@@ -600,21 +611,11 @@ class _MyGoalWidgetState extends State<MyGoalWidget> {
                               Navigator.pop(context);
                             },
                             child: const Text('Delete Goal'),
-                      ),
+                          ),
                           IconButton(
-                          onPressed: (){myController.text='';Navigator.pop(context);},
-                          icon: const Icon(Icons.close_sharp)
-                      ),
-                          if(widget.goalName![0] != '!')TextButton(
-                            onPressed: (){},
-                            onLongPress:(){
-                              goalsMap['! ${widget.goalName}']=goalsMap[widget.goalName];
-                              goalsMap.remove(widget.goalName);
-                              widget.updateState();
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Achieved')
-                          )
+                              onPressed: (){myController.text='';Navigator.pop(context);},
+                              icon: const Icon(Icons.close_sharp)
+                          ),
                         ],
                       )
                     ],
@@ -717,8 +718,7 @@ class _MyMilestonesState extends State<MyMilestones> {
                         child: const Text('Achieved',),
                         onPressed: (){},
                         onLongPress:(){
-                          goalsMap[widget.goalName]!.add('! ${widget.myTask}');
-                          goalsMap[widget.goalName]!.remove(widget.myTask);
+                          goalsMap[widget.goalName]![goalsMap[widget.goalName]!.indexOf(widget.myTask!)]='! ${widget.myTask}';
                           widget.updateState();
                           Navigator.pop(context);
                         }
@@ -854,7 +854,7 @@ class _MyScoresState extends State<MyScores> {
       height: double.infinity,
       padding: const EdgeInsets.all(10),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Center(child:Text(timeInfo.year.toString(),style: const TextStyle(fontSize: 50,color: Colors.white),)),
           Container(child:Row(
@@ -891,17 +891,16 @@ class KermDatabase {
     await _db.insert('KermData', {'id': 2, 'ltg': '9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9-9', 'stg': ''}, conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
-  Future<void> updateGoalData(Map<String,List<String>?> myGoalsMap)async{
+  Future<void> updateGoalData()async{
     Database db = await openDB();
     Map<String,dynamic> myMap={'id':0};
     List<String?> ltg=[],stg=[];
-
-    myGoalsMap.forEach((key, value) {
+    goalsMap.forEach((key, value) {
       ltg.add(key);
       if(value!.isEmpty) {
         stg.add('Egg');
       } else {
-        stg.add(value.join('-'));
+        stg.add(value.join('\t'));
       }
     });
 
@@ -939,7 +938,7 @@ class KermDatabase {
 
     for (int i =0; i < ltg!.length ; i++) {
       if(stg![i]!='Egg'){
-        goalMap[ltg[i]] = stg[i].split('-');
+        goalMap[ltg[i]] = stg[i].split('\t');
       } else {
         goalMap[ltg[i]] = [];
       }
@@ -953,7 +952,6 @@ class KermDatabase {
   Future<void> updateQuestData()async{
     Database db = await openDB();
     Map<String,dynamic> myMap = {'id':1};
-
     print(questList);
 
     List<String> ltg = [];
